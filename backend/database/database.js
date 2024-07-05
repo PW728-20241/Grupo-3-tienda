93bc938cb58d3baf6a15eeda035f7ba83628f436
HollowedRino <20200865@aloe.ulima.edu.pe>
2024-07-05 15:00:11 -0500

    index.js actualizado

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("tercerintento", "tercerintento", "Abc$1234", {
-    host: "tercerintento.postgres.database.azure.com",
+export const sequelize = new Sequelize("grupotres", "grupotres", "Abc$1234", {
+    host: "grupotres.postgres.database.azure.com",
     dialect: "postgres"
 });
